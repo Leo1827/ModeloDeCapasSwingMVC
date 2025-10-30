6af46b9c00414c193041f04c85dcb4863dc90e93
Mileer leon <mileerdubanleon@gmail.com>
2025-10-30 13:30:18 -0500

    feat: add model persona

--- src/IGU/Persona.java
+++ src/IGU/Persona.java
@@ -1,4 +1,19 @@
 package IGU;
 
 public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() { return nombre;}
+    public int getEdad() { return edad; }
+
+    @Override
+    public String toString() {
+        return nombre + "-" + edad + "años";
+    }
 }
